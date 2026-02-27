b4ce62bfdf574375ad4eaa51e5eeafc032e5330e
Ayisha-Siyavudeen <siyavudeenayisha@gmail.com>
2026-02-27 23:27:36 +0530

    add

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function showMessage() {
     alert("Welcome to VolunGo!");
-
-    
 }
+
+ function register() {
+    alert("Volunteer Registered Successfully!");
+}
+function postOpportunity() {
+    alert("Opportunity Posted Successfully!");
+}
+function apply() {
+    window.location.href = "volunteer.html"; // redirect to registration page
+}
+
+
